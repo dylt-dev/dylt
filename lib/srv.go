52d023802ca2e3795d73d2dd5259a4da996138ae
Chris Lalos <chris.lalos@gmail.com>
2024-05-02 14:42:46 -0500

    lil refactor

---
+++ lib/srv.go
@@ -0,0 +1,39 @@
+package lib
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+type Srv struct {
+	net.SRV
+	Ips []string
+}
+
+func GetSrvs (domain string, includeIps bool) []Srv {
+	var srvs []Srv
+	// Use "net" package to do a DNS lookup of SRVs 
+	_, dnsSrvs, err := net.LookupSRV("etcd-server", "tcp", domain)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	// For each DNS SRV record, create an Srv object
+	// Optionally populate the srv Ips field if --include-ips was set
+	for _, dnsSrv := range dnsSrvs {
+		srv := Srv{SRV: *dnsSrv}
+		if includeIps {
+			ips, err := net.LookupHost(dnsSrv.Target)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				srv.Ips = []string{}
+			} else {
+				srv.Ips = ips
+			}
+		}
+		srvs = append(srvs, srv)
+	}
+
+	return srvs
+}
