bb59504678bd04209785d431b20c06e72354b4fa
Chris Lalos <chris.lalos@gmail.com>
2024-05-09 21:54:22 -0500

    .gitignore

--- lib/etcdutil_test.go
+++ lib/etcdutil_test.go
@@ -1,8 +1,11 @@
 package lib
 
 import (
+	"context"
 	"fmt"
 	"testing"
+
+	clientV3 "go.etcd.io/etcd/client/v3"
 )
 
 
@@ -18,4 +21,21 @@ func TestEtcdGet (t *testing.T) {
 		t.Fatal("EtcdGet() failed", err)
 	}
 	fmt.Println(string(val))
+}
+
+
+func TestEtcdGetWithPrefix (t *testing.T){
+	cli, err := NewEtcdClient("hello.dylt.dev")
+	if err != nil {
+		t.Fatal("NewEtcdClient() failed", err)
+	}
+	resp, err := cli.Client.Get(context.Background(), "/vm", clientV3.WithPrefix())
+	if err != nil {
+		t.Fatal("Client.Get() failed", err)
+	}
+	t.Logf("Count    = %d", resp.Count)
+	t.Logf("len(Kvs) = %d", len(resp.Kvs))
+	for i, kv := range(resp.Kvs) {
+		t.Logf("Kvs[%d].Version = %s\n", i, kv.Key)
+	}
 }
