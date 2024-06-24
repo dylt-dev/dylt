4d9985620e3f680eb3cdd68b0066f5d090f9eee8
lalos <chris.lalos@gmail.com>
2024-06-24 08:49:35 -0500

    fixed failure on startup

--- main.go
+++ main.go
@@ -6,14 +6,16 @@ import (
 	"path"
 
 	"github.com/dylt-dev/dylt/cli"
-	"github.com/dylt-dev/dylt/lib"
+//	"github.com/dylt-dev/dylt/lib"
 )
 
+const LOG_File = "dylt.log"
+const LOG_Folder = "/var/log/dylt"
 
 func initLogging () {
-	logFile := "dylt.log"
-	logFolder := "/var/log/dylt"
-	logPath := path.Join(logFolder, logFile)
+	err := os.MkdirAll(LOG_Folder, 0744)
+	if err != nil { panic("Couldn't create or open log folder") }
+	logPath := path.Join(LOG_Folder, LOG_File)
 	logWriter, err := os.OpenFile(logPath, os.O_CREATE | os.O_APPEND | os.O_RDWR, 0777)
 	if err != nil { panic("Couldn't open logfile") }
 	opts := slog.HandlerOptions{AddSource: true}
@@ -23,7 +25,7 @@ func initLogging () {
 
 
 func main () {
-	lib.InitConfig()
+	// lib.InitConfig()
 	initLogging()
 	os.Exit(cli.Run())
 }
