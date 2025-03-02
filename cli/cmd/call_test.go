49a7dac2393f6a378757fc0d91f950b082835e57
Chris Lalos <chris.lalos@gmail.com>
2025-03-01 20:02:35 -0600

    Added unit + sys test for `dylt call`

---
+++ cli/cmd/call_test.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/dylt-dev/dylt/lib"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRunCall0 (t *testing.T) {
+	scriptPath := "/tmp/daylight.sh"
+	scriptArgs := []string{"hello"}
+	assert.FileExists(t, scriptPath)
+	err := RunCall(scriptPath, scriptArgs)
+	assert.Nil(t, err)
+}
+
+func TestRunCallCmd0 (t *testing.T) {
+	sCmdline := "/Users/chris/src/dylt-dev/dylt/dylt call --script-path /tmp/daylight.sh hello"
+	var cmdline Cmdline = strings.Split(sCmdline, " ")
+	t.Log("cmdline.Command()", cmdline.Command(), "cmdline.Args()", cmdline.Args())
+	rc, stdout, err := lib.RunCommand(cmdline.Command(), cmdline.Args()...)
+	assert.Equal(t, 0, rc)
+	assert.NotEmpty(t, stdout)
+	assert.Nil(t, err)
+	t.Log(string(stdout))
+}
+
+func TestRunCallNoScript (t *testing.T) {
+	sCmdline := "XXX call --script-path /tmp/daylight.sh hello"
+	var cmdline Cmdline = strings.Split(sCmdline, " ")
+	t.Log("cmdline.Command()", cmdline.Command(), "cmdline.Args()", cmdline.Args())
+	rc, stdout, err := lib.RunCommand(cmdline.Command(), cmdline.Args()...)
+	assert.NotEqual(t, 0, rc)
+	assert.Empty(t, stdout)
+	assert.NotNil(t, err)
+	t.Log(string(stdout))
+}
