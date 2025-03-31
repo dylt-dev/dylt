01f195605ce242e32e4be37305e69fd36efb5025
Chris Lalos <chris.lalos@gmail.com>
2025-03-30 23:22:44 -0500

    eco is looking good for initial encoding, up to slice. map+struct or next.

--- common/eco.go
+++ common/eco.go
@@ -1,1 +1,488 @@
 package common
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"path/filepath"
+	"reflect"
+	"strconv"
+	"strings"
+
+	etcd "go.etcd.io/etcd/client/v3"
+)
+
+func Encode(ctx *ecoContext, key string, i any) ([]etcd.Op, error) {
+	var ty reflect.Type = reflect.TypeOf(i)
+	// var _ reflect.Value = reflect.ValueOf(i)
+	var ops = []etcd.Op{}
+	var kind reflect.Kind = ty.Kind()
+	var val reflect.Value = reflect.ValueOf(i)
+	var value []byte
+	var err error
+	switch kind {
+	case reflect.Bool,
+		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Float32,
+		reflect.Float64,
+		reflect.String:
+		value, err = json.Marshal(i)
+		if err != nil {
+			return nil, err
+		}
+		opPut := etcd.OpPut(key, string(value))
+		ops = append(ops, opPut)
+	case reflect.Array:
+		opsArray, err := encodeSlice(ctx, key, val)
+		if err != nil { return nil, err }
+		ops = append(ops, opsArray...)
+	case reflect.Map:
+		opsMap, err := encodeMap(ctx, key, val)
+		if err != nil { return nil, err }
+		ops = append(ops, opsMap...)
+	case reflect.Slice:
+		opsSlice, err := encodeSlice(ctx, key, val)
+		if err != nil { return nil, err }
+		ops = append(ops, opsSlice...)
+	case reflect.Struct:
+		opsStruct, err := encodeStruct(ctx, key, val)
+		if err != nil { return nil, err }
+		ops = append(ops, opsStruct...)
+	default:
+		err = errors.New("unsupported")
+		return nil, err
+	}
+	return ops, nil
+}
+
+type ecoContext struct {
+	context.Context
+	level int
+}
+
+func newEcoContext() *ecoContext {
+	return &ecoContext{Context: context.Background(), level: 0}
+}
+
+func (ctx *ecoContext) dec () *ecoContext {
+	ctx.level--
+	return ctx
+}
+
+func (ctx *ecoContext) inc () *ecoContext {
+	ctx.level++
+	return ctx
+}
+
+func (ctx *ecoContext) indent () string {
+	return strings.Repeat("  ", ctx.level)
+}
+
+func (ctx *ecoContext) printf(format string, a ...any) (int, error) {
+	format = fmt.Sprintf("%s%s", ctx.indent(), format)
+	return fmt.Printf(format, a...)
+}
+
+func (ctx *ecoContext) println(a ...any) (int, error) {
+	args := fmt.Sprintln(a...)
+	return fmt.Printf("%s%s", ctx.indent(), args)
+}
+
+type kind uint
+
+const (
+	Invalid kind = iota
+	Bool
+	Number
+	String
+	SimpleArray
+	SimpleInterface
+	SimpleMap
+	SimplePointer
+	SimpleSlice
+	SimpleStruct
+)
+
+func (k kind) String() string {
+	switch k {
+	case Invalid:
+		return "Invalid"
+	case Bool:
+		return "Bool"
+	case Number:
+		return "Number"
+	case String:
+		return "String"
+	case SimpleArray:
+		return "SimpleArray"
+	case SimpleInterface:
+		return "SimpleInterface"
+	case SimpleMap:
+		return "SimpleMap"
+	case SimplePointer:
+		return "SimplePointer"
+	case SimpleSlice:
+		return "SimpleSlice"
+	case SimpleStruct:
+		return "SimpleStruct"
+	default:
+		return fmt.Sprintf("Unknown kind :%d", k)
+	}
+}
+
+func arrayKind(ctx *ecoContext, ty reflect.Type) kind {
+	ctx.printf("In arrayKind(): ty=%s\n", fullTypeName(ty))
+	ctx.inc()
+	defer ctx.dec()
+
+	if ty.Kind() != reflect.Array {
+		ctx.println("type is not a array; returning Invalid")
+		return Invalid
+	}
+	
+	tyElem := ty.Elem()
+	ctx.printf("Checking element type (%s) ... ", fullTypeName(tyElem))
+	if isTypeSimple(ty.Elem()) {
+		ctx.println("element type is simple; returning SimpleArray", fullTypeName(tyElem))
+		return SimpleArray
+	}
+	ctx.println("conditions were not met; returning Invalid")
+	return Invalid
+}
+
+func encodeMap (ctx *ecoContext, key string, val reflect.Value) ([]etcd.Op, error) {
+	return nil, errors.New("tbd")
+}
+
+func encodeSlice (ctx *ecoContext, key string, val reflect.Value) ([]etcd.Op, error) {
+	ctx.printf("encodeSlice() - key=%s", key)
+	ctx.inc()
+	defer ctx.dec()
+
+	ty := val.Type()
+	if getTypeKind(ctx, ty) != SimpleSlice {
+		return nil, fmt.Errorf("expecting SimpleSlice; got %s", fullTypeName(ty))
+	}
+
+	j, err := json.Marshal(val.Interface())
+	if err != nil { return nil, err }
+	op := etcd.OpPut(key, string(j))
+	
+	return []etcd.Op{op}, nil
+}
+
+func encodeStruct (ctx *ecoContext, key string, val reflect.Value) ([]etcd.Op, error) {
+	return nil, errors.New("tbd")
+}
+
+func fullTypeName(ty reflect.Type) string {
+	pkgPath := ty.PkgPath()
+	typeName := ty.Name()
+	if typeName == "" {
+		typeName = "(anon)"
+	}
+
+	if pkgPath == "" {
+		return typeName
+	}
+
+	if filepath.Dir(pkgPath) == "github.com/dylt-dev/dylt" {
+		pkgPath = filepath.Base(pkgPath)
+	}
+
+	return fmt.Sprintf("%s.%s", pkgPath, typeName)
+}
+
+func getFieldName(sf reflect.StructField) string {
+	tagValue, ok := sf.Tag.Lookup("eco")
+	var fieldName string
+	if ok {
+		fieldName = tagValue
+	} else {
+		fieldName = sf.Name
+	}
+
+	return fieldName
+}
+
+func getFieldValue(val reflect.Value) (string, error) {
+	var s string
+	kind := val.Type().Kind()
+	switch kind {
+	case reflect.Bool:
+		s = strconv.FormatBool(val.Bool())
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		s = strconv.FormatInt(val.Int(), 10)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		s = strconv.FormatUint(val.Uint(), 10)
+	case reflect.Float32:
+		s = strconv.FormatFloat(val.Float(), 'f', -1, 32)
+	case reflect.Float64:
+		s = strconv.FormatFloat(val.Float(), 'f', -1, 64)
+	case reflect.String:
+		s = string(val.String())
+	default:
+		return "", fmt.Errorf("unsupported kind: %s", kind.String())
+	}
+
+	return s, nil
+}
+
+func getKind (ctx *ecoContext, i any) kind {
+	// ty := reflect.TypeOf(i)
+	// if fullTypeName(ty) == "reflect.Type" {
+	// 	fmt.Println("Warning - GetKind() called with reflect.Type(). Did you mean GetTypeKind()?")
+	// }
+	return getTypeKind(ctx, reflect.TypeOf(i))
+}
+
+func getTypeKind(ctx *ecoContext, ty reflect.Type) kind {
+	reflectKind := ty.Kind()
+	// fmt.Printf("ty=%s reflectKind=%s\n", fullTypeName(ty), reflectKind.String())
+	switch reflectKind {
+	case reflect.Bool:
+		return Bool
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr,
+		reflect.Float32,
+		reflect.Float64:
+		return Number
+	case reflect.String:
+		return String
+	case reflect.Array:
+		return arrayKind(ctx, ty)
+	case reflect.Interface:
+		return interfaceKind(ctx, ty)
+	case reflect.Map:
+		return mapKind(ctx, ty)
+	case reflect.Pointer:
+		return pointerKind(ctx, ty)
+	case reflect.Slice:
+		return sliceKind(ctx, ty)
+	case reflect.Struct:
+		return structKind(ctx, ty)
+	case reflect.Chan, reflect.Complex64, reflect.Complex128, reflect.Func, reflect.UnsafePointer:
+		return Invalid
+	default:
+		return Invalid
+	}
+}
+
+func interfaceKind(ctx *ecoContext, ty reflect.Type) kind {
+	ctx.printf("In interfaceKind(): ty=%s\n", fullTypeName(ty))
+	ctx.inc()
+	defer ctx.dec()
+
+	if ty.Kind() != reflect.Interface {
+		ctx.println("type is not an interface; returning Invalid")
+		return Invalid
+	}
+
+	return Invalid
+}
+
+func isSimple(kind reflect.Kind) bool {
+	switch kind {
+	case reflect.Bool,
+		 reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+	     reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		 reflect.Float32, reflect.Float64,
+		 reflect.String:
+		 return true
+	default:
+		return false 
+	}
+}
+
+func isTypeSimple(ty reflect.Type) bool {
+	return isSimple(ty.Kind())
+}
+
+func mapKind(ctx *ecoContext, ty reflect.Type) kind {
+	ctx.printf("In mapKind(): ty=%s\n", fullTypeName(ty))
+	ctx.inc()
+	defer ctx.dec()
+
+	if ty.Kind() != reflect.Map {
+		ctx.println("type is not a map; returning Invalid")
+		return Invalid
+	}
+
+	ctx.printf("Checking key (%s) ... ", fullTypeName(ty.Key()))
+	if !isTypeSimple(ty.Key()) {
+		ctx.printf("non-simple; returning Invalid\n")
+		return Invalid
+	}
+	ctx.printf("key type is simple; continuing\n")
+
+	tyElem := ty.Elem()
+	ctx.printf("Checking element type (%s) ...\n", fullTypeName(tyElem))
+	ctx.inc()
+	kindElem := getTypeKind(ctx, tyElem)
+	ctx.dec()
+
+	if isTypeSimple(tyElem) {
+		ctx.printf("element type is simple; returning SimpleMap\n")
+		return SimpleMap
+	}
+
+	if kindElem == SimpleMap ||
+		kindElem == SimpleStruct ||
+		kindElem == SimpleSlice {
+		ctx.printf("element Kind is %s; returning SimpleMap\n", kindElem.String())
+		return SimpleMap
+	}
+
+	ctx.println("conditions were not met; returning Invalid")
+	return Invalid
+}
+
+func pointerKind(ctx *ecoContext, ty reflect.Type) kind {
+	ctx.printf("In pointerKind(): ty=%s\n", fullTypeName(ty))
+	ctx.inc()
+	defer ctx.dec()
+
+	if ty.Kind() != reflect.Pointer {
+		ctx.println("type is not a pointer; returning Invalid")
+		return Invalid
+	}
+
+	tyElem := ty.Elem()
+	ctx.printf("Checking pointer type (%s) ... ", fullTypeName(tyElem))
+	if isTypeSimple(tyElem) {
+		ctx.println("pointer type is simple; returning SimplePointer")
+		return SimplePointer
+	}
+
+	ctx.println("conditions were not met; returning Invalid")
+	return Invalid
+}
+
+func sliceKind(ctx *ecoContext, ty reflect.Type) kind {
+	ctx.printf("In sliceKind(): ty=%s\n", fullTypeName(ty))
+	ctx.inc()
+	defer ctx.dec()
+
+	if ty.Kind() != reflect.Slice {
+		ctx.println("type is not a slice; returning Invalid")
+		return Invalid
+	}
+
+	tyElem := ty.Elem()
+	ctx.printf("Checking element type (%s) ... ", fullTypeName(tyElem))
+	if isTypeSimple(tyElem) {
+		ctx.println("element type is simple; returning SimpleSlice")
+		return SimpleSlice
+	}
+
+	ctx.println("conditions were not met; returning Invalid")
+	return Invalid
+}
+
+func structKind(ctx *ecoContext, ty reflect.Type) kind {
+	ctx.printf("In structKind(): ty=%s\n", fullTypeName(ty))
+	ctx.inc()
+	defer ctx.dec()
+
+	if ty.Kind() != reflect.Struct {
+		ctx.println("type is not a struct; returning Invalid")
+		return Invalid
+	}
+
+	ctx.printf("%d field(s)\n", ty.NumField())
+	for i := range ty.NumField() {
+		sf := ty.Field(i)
+		sfType := sf.Type
+		ctx.printf("checking field '%s': type name = %s ...\n", sf.Name, fullTypeName(sfType))
+		sfReflectKind := sfType.Kind()
+
+		if isTypeSimple(sfType) {
+			ctx.println("field type is simple; continuing")
+			continue
+		}
+
+		if sfReflectKind == reflect.Map && mapKind(ctx, sfType) == SimpleMap {
+			ctx.println("field type is SimpleMap; continuing")
+			continue
+		}
+
+		if sfReflectKind == reflect.Slice && sliceKind(ctx, sfType) == SimpleSlice {
+			ctx.println("field type is SimpleSlice; continuing")
+			continue
+		}
+
+		if sfReflectKind == reflect.Struct && structKind(ctx, sf.Type) == SimpleStruct {
+			ctx.println("field type is SimpleStruct; continuing")
+			continue
+		}
+
+		return Invalid
+	}
+
+	ctx.println("All fields passed; returning SimpleStruct")
+	return SimpleStruct
+}
+
+func writeSlice (ctx *ecoContext, builder *strings.Builder, val *reflect.Value) (*strings.Builder, error) {
+	ctx.println("writeValue()")
+	ctx.inc()
+	defer ctx.dec()
+
+	ty := val.Type()
+	if getTypeKind(ctx, ty) != SimpleSlice {
+		return nil, fmt.Errorf("invalid type (%s); expected SimpleSlice", fullTypeName(ty))
+	}
+	len := val.Len()
+	ctx.printf("len=%d\n", len)
+	builder.WriteRune('[')
+	for i := range len {
+		el := val.Index(i)
+		inf := el.Interface()
+		b, err := json.Marshal(inf)
+		if err != nil { return nil, err }
+		if i > 0 {
+			_, err = builder.WriteRune(',')
+			if err != nil { return nil, err }
+		}
+		_, err = builder.Write(b)
+		if err != nil { return nil, err }
+	}
+	builder.WriteRune(']')
+
+	return builder, nil
+}
+
+
+	/*
+	var i any
+	switch val.Type().Kind() {
+	case reflect.Bool:
+		i = val.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return intEncoder
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return uintEncoder
+	case reflect.Float32:
+		return float32Encoder
+	case reflect.Float64:
+		return float64Encoder
+	case reflect.String:
+		return stringEncoder
+	case reflect.Interface:
+		return interfaceEncoder
+	case reflect.Struct:
+		return newStructEncoder(t)
+	case reflect.Map:
+		return newMapEncoder(t)
+	case reflect.Slice:
+		return newSliceEncoder(t)
+	case reflect.Array:
+		return newArrayEncoder(t)
+	case reflect.Pointer:
+		return newPtrEncoder(t)
+	default:
+		return unsupportedTypeEncoder
+	}
+*/
--- common/eco_test.go
+++ common/eco_test.go
@@ -1,37 +1,454 @@
 package common
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"path/filepath"
+	"reflect"
 	"strconv"
 	"testing"
+	"unsafe"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	etcd3 "go.etcd.io/etcd/client/v3"
+	etcd "go.etcd.io/etcd/client/v3"
 )
 
+type emptyStruct struct{}
+type arrayOfInt [0]int
+type arrayOfStruct [0]emptyStruct
+type mapOfIntSlice map[string][]int
+type pointerToInt *int
+type sliceOfInt []int
+type sliceOfStruct []emptyStruct
+var VAL_MapSimple = map[string]int{}
+var VAL_MapUnsimple = map[struct{}]struct{}{}
+var VAL_MapUnsimpleKey = map[struct{}]int{}
+var VAL_MapUnsimpleValue = map[int]struct{}{}
+var VAL_SliceSimple = []int{5, 8, 13}
+var VAL_SliceUnsimple = []struct{}{}
+var VAL_SimplePointer = new(int)
+var VAL_UnsimplePointer = &(struct{}{})
+
+var VAL_MapWithStructKey = map[EcoTest]string{}
+var VAL_MapWithStructValue = map[string]EcoTest{}
+
 type EcoTest struct {
-	Name string `eco:"Name"`
-	LuckyNumber float64 `echo:"LuckyNumber"`
+	Name        string  `eco:"name"`
+	LuckyNumber float64 `eco:"lucky_number"`
+	Anon        string
 }
 
-func NewEcoTest (name string, luckyNumber float64) *EcoTest {
+type structWithMap struct {
+	M map[int]string
+}
+
+type UnsimpleStruct struct {
+	C complex64
+	F func()
+}
+
+func NewEcoTest(name string, luckyNumber float64) *EcoTest {
 	return &EcoTest{Name: name, LuckyNumber: luckyNumber}
 }
 
-func TestGetObject (t *testing.T) {
-	
+func TestEncodeEtcd_Bool(t *testing.T) {
+	key := "key"
+	i := false
+	testEncodeBool(t, key, i)
 }
 
-func TestPutObject (t *testing.T) {
+func TestEncodeEtcd_Float(t *testing.T) {
+	key := "key"
+	i := 42.0
+	testEncodeNumber(t, key, i)
+}
+func TestEncodeEtcd_Int(t *testing.T) {
+	key := "key"
+	i := 13
+	testEncodeNumber(t, key, i)
+}
+
+func TestEncodeEtcd_IntSlice(t *testing.T) {
+	ctx := newEcoContext()
+	slice := []int{5, 8, 13}
+	j, err := json.Marshal(slice)
+	assert.NoError(t, err)
+	sJson := string(j)
+
+	key := "key"
+	ops, err := encodeSlice(ctx, key, reflect.ValueOf(slice))
+	dumpOps(t, ops)
+	assert.NoError(t, err)
+	assert.Equal(t, key, string(ops[0].KeyBytes()))
+	assert.Equal(t, sJson, string(ops[0].ValueBytes()))
+}
+
+func TestEncodeEtcd_String(t *testing.T) {
+	key := "key"
+	i := "foo"
+	testEncodeString(t, key, i)
+}
+
+func TestEncoding0(t *testing.T) {
+	var s = `"8 is < g but > 13"`
+	var buf []byte
+	var err error
+	buf, err = json.Marshal(s)
+	assert.NoError(t, err)
+	assert.NotNil(t, buf)
+	t.Logf("%-20s %s", "Marshalled s", string(buf))
+	bb := bytes.NewBuffer([]byte{})
+	encoder := json.NewEncoder(bb)
+	encoder.SetEscapeHTML(false)
+	err = encoder.Encode(s)
+	assert.NoError(t, err)
+	t.Logf("%-20s %s", "Encoded s", bb.String())
+}
+
+func TestGetObject(t *testing.T) {
+
+}
+
+func TestKind_ArrayOfInt(t *testing.T) {
+	kind := getKind(newEcoContext(), arrayOfInt{})
+	assert.Equal(t, SimpleArray, kind)
+}
+
+func TestKind_ArrayOfStruct(t *testing.T) {
+	kind := getKind(newEcoContext(), arrayOfStruct{})
+	assert.Equal(t, Invalid, kind)
+}
+
+func TestKind_MapOfIntSlice(t *testing.T) {
+	kind := getKind(newEcoContext(), mapOfIntSlice{})
+	assert.Equal(t, SimpleMap, kind)
+
+}
+
+func TestKind_MapOfSliceOfStruct(t *testing.T) {
+	type emptyStruct struct{}
+	type mapOfSlice map[string][]emptyStruct
+	kind := getKind(newEcoContext(), mapOfSlice{})
+	assert.Equal(t, Invalid, kind)
+}
+
+func TestKind_MapSimple(t *testing.T) {
+	kind := getKind(newEcoContext(), VAL_MapSimple)
+	assert.Equal(t, SimpleMap, kind)
+}
+
+func TestKind_MapUnsimple(t *testing.T) {
+	kind := getKind(newEcoContext(), VAL_MapUnsimple)
+	assert.Equal(t, Invalid, kind)
+}
+
+func TestKind_MapUnsimpleKey(t *testing.T) {
+	kind := getKind(newEcoContext(), VAL_MapUnsimpleKey)
+	assert.Equal(t, Invalid, kind)
+}
+
+func TestKind_MapUnsimpleValue(t *testing.T) {
+	kind := getKind(newEcoContext(), VAL_MapUnsimpleValue)
+	assert.Equal(t, SimpleMap, kind)
+}
+
+func TestKind_PointerToInt(t *testing.T) {
+	var pint pointerToInt
+	kind := getKind(newEcoContext(), pint)
+	assert.Equal(t, SimplePointer, kind)
+}
+
+func TestKind_PointerToIntSlice(t *testing.T) {
+	kind := getKind(newEcoContext(), new(sliceOfInt))
+	assert.Equal(t, SimplePointer, kind)
+}
+
+func TestKind_PointerToStructSlice(t *testing.T) {
+	kind := getKind(newEcoContext(), new(sliceOfStruct))
+	assert.Equal(t, Invalid, kind)
+}
+
+func TestKind_SliceSimple(t *testing.T) {
+	type intSlice []int
+	i := intSlice{1, 2, 3}
+	kind := getKind(newEcoContext(), i)
+	assert.Equal(t, SimpleSlice, kind)
+}
+
+func TestKind_SliceOfMap(t *testing.T) {
+	type simpleMap map[int]int
+	type sliceOfMap []simpleMap
+	i := sliceOfMap{}
+	kind := getKind(newEcoContext(), i)
+	assert.Equal(t, Invalid, kind)
+}
+
+func TestKind_SliceUnsimple(t *testing.T) {
+	type emptyStruct struct{}
+	type emptyStructSlice []emptyStruct
+	i := emptyStructSlice{}
+	kind := getKind(newEcoContext(), i)
+	assert.Equal(t, Invalid, kind)
+}
+
+func TestKind_StructSimple(t *testing.T) {
+	kind := getKind(newEcoContext(), EcoTest{})
+	assert.Equal(t, SimpleStruct, kind)
+}
+
+func TestKind_StructUnsimple(t *testing.T) {
+	kind := getKind(newEcoContext(), UnsimpleStruct{})
+	assert.Equal(t, Invalid, kind)
+}
+
+func TestKind_StructWithMap(t *testing.T) {
+	kind := getKind(newEcoContext(), structWithMap{})
+	assert.Equal(t, SimpleStruct, kind)
+}
+
+func TestKind_StructWithSlice(t *testing.T) {
+	type structWithSlice struct{ Slice []int }
+	kind := getKind(newEcoContext(), structWithSlice{})
+	assert.Equal(t, SimpleStruct, kind)
+}
+
+func TestKind_StructWithMapWithSlice(t *testing.T) {
+	type mapWithSlice map[string][]int
+	type structWithMapWithSlice struct{ M mapWithSlice }
+	kind := getKind(newEcoContext(), structWithMapWithSlice{})
+	assert.Equal(t, SimpleStruct, kind)
+}
+
+func TestKind_StructWithMapWithStruct(t *testing.T) {
+	type innerStruct struct{}
+	type mapWithStruct map[string]innerStruct
+	type structWithMapWithSlice struct{ MapField mapWithStruct }
+	kind := getKind(newEcoContext(), structWithMapWithSlice{})
+	assert.Equal(t, SimpleStruct, kind)
+}
+
+func TestPutObject0(t *testing.T) {
 	etcdClient, err := NewEtcdClientFromConfig()
 	require.NoError(t, err)
 	require.NotNil(t, etcdClient)
 	obj := NewEcoTest("Me", 13)
 
 	prefix := "/_test_/echotest"
-	opName := etcd3.OpPut("Name", filepath.Join(prefix, obj.Name))
-	opLuckyNumber := etcd3.OpPut("LuckyNumber", filepath.Join(prefix, strconv.FormatFloat(obj.LuckyNumber, 'f', 8, 64)))
+
+	ops := []etcd.Op{}
+	opName := etcd.OpPut(filepath.Join(prefix, "Name"), obj.Name, etcd.WithPrevKV())
+	ops = append(ops, opName)
+	opLuckyNumber := etcd.OpPut(filepath.Join(prefix, "LuckyNumber"), strconv.FormatFloat(obj.LuckyNumber, 'f', 8, 64), etcd.WithPrevKV())
+	ops = append(ops, opLuckyNumber)
+
 	txn := etcdClient.Txn(context.Background())
-	txn.Then(opName, opLuckyNumber).Commit()
+	resp, err := txn.Then(ops...).Commit()
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+	for _, respOp := range resp.Responses {
+		t.Logf("respOp.GetResponsePut().PrevKV: %s => %s", respOp.GetResponsePut().PrevKv.Key, respOp.GetResponsePut().PrevKv.Value)
+	}
+}
+
+func TestPutObject1(t *testing.T) {
+	etcdClient, err := NewEtcdClientFromConfig()
+	require.NoError(t, err)
+	require.NotNil(t, etcdClient)
+	obj := NewEcoTest("Me", 13)
+
+	prefix := "/_test_/echotest"
+
+	ops := []etcd.Op{}
+	opName := etcd.OpPut(filepath.Join(prefix, "Name"), obj.Name, etcd.WithPrevKV())
+	ops = append(ops, opName)
+	opLuckyNumber := etcd.OpPut(filepath.Join(prefix, "LuckyNumber"), strconv.FormatFloat(obj.LuckyNumber, 'f', 8, 64), etcd.WithPrevKV())
+	ops = append(ops, opLuckyNumber)
+
+	txn := etcdClient.Txn(context.Background())
+	resp, err := txn.Then(ops...).Commit()
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+	for _, respOp := range resp.Responses {
+		// t.Logf("respOp=%#v", respOp)
+		// t.Logf("respOp.GetResponsePut()=%#v", respOp.GetResponsePut())
+		// t.Logf("respOp.GetResponsePut()=%#v", respOp.GetResponsePut())
+		// t.Logf("respOp.GetResponsePut()=%#v", respOp.GetResponsePut())
+		// t.Logf("respOp.GetResponsePut().PrevKv=%#v", respOp.GetResponsePut().PrevKv)
+		t.Logf("respOp.GetResponsePut().PrevKV: %s => %s", respOp.GetResponsePut().PrevKv.Key, respOp.GetResponsePut().PrevKv.Value)
+
+	}
+}
+
+func TestReflection0(t *testing.T) {
+	obj := NewEcoTest("Me", 13)
+
+	var ty reflect.Type
+	var val reflect.Value
+	ty = reflect.TypeOf(obj)
+	if ty.Kind() == reflect.Pointer {
+		t.Log("var is a pointer - dereferencing")
+		ty = reflect.TypeOf(*obj)
+		val = reflect.ValueOf(*obj)
+	} else {
+		val = reflect.ValueOf(obj)
+	}
+	assert.Equal(t, 3, ty.NumField())
+
+	for i := range ty.NumField() {
+		sf := ty.Field(i)
+		var s string
+		switch sf.Type.Kind() {
+		case reflect.Float32:
+			s = strconv.FormatFloat(val.Field(i).Float(), 'f', -1, 32)
+		case reflect.Float64:
+			s = strconv.FormatFloat(val.Field(i).Float(), 'f', -1, 64)
+		case reflect.String:
+			s = string(val.Field(i).String())
+		default:
+			s = "N/A"
+		}
+		t.Logf("%s=%s", getFieldName(sf), s)
+	}
+}
+
+func TestReflection1(t *testing.T) {
+	obj := NewEcoTest("Me", 13)
+
+	ty, val, err := reflectStruct(obj)
+	require.NoError(t, err)
+	require.NotNil(t, ty)
+	require.NotEmpty(t, val)
+	dumpStruct(t, ty, val)
+}
+
+func TestReflection2(t *testing.T) {
+	obj := NewEcoTest("Me", 13)
+	var ptr **EcoTest = &obj
+	var pptr ***EcoTest = &ptr
+	var ppptr ****EcoTest = &pptr
+
+	ty, val, err := reflectStruct(ppptr)
+	require.NoError(t, err)
+	require.NotNil(t, ty)
+	require.NotEmpty(t, val)
+	require.Equal(t, 2, ty.NumField())
+	dumpStruct(t, ty, val)
+}
+
+func TestReflection3(t *testing.T) {
+	type selfish *selfish
+	var obj selfish
+	obj = &obj
+	ty, val, err := reflectStruct(obj)
+	require.Nil(t, ty)
+	require.Empty(t, val)
+	require.Error(t, err)
+}
+
+func reflectStruct(obj any) (reflect.Type, reflect.Value, error) {
+	ty := reflect.TypeOf(obj)
+	val := reflect.ValueOf(obj)
+	var lastPtr uintptr = uintptr(unsafe.Pointer(&val))
+	return reflectStructNoCycle(ty, val, lastPtr)
+}
+
+func reflectStructNoCycle(ty reflect.Type, val reflect.Value, lastPtr uintptr) (reflect.Type, reflect.Value, error) {
+	switch ty.Kind() {
+	case reflect.Struct:
+		return ty, val, nil
+	case reflect.Pointer:
+		fmt.Printf("val: %#x\n", val.Pointer())
+		ptr := val.Pointer()
+		val = reflect.Indirect(val)
+		ty = val.Type()
+		if ptr == lastPtr {
+			return nil, reflect.Value{}, errors.New("cycle detected")
+		}
+		return reflectStructNoCycle(ty, val, ptr)
+	case reflect.UnsafePointer:
+		fmt.Printf("val: %p\n", val.Pointer)
+		val = reflect.NewAt(ty, unsafe.Pointer(&val))
+		fmt.Printf("ty: %s.%s\n", ty.PkgPath(), ty.Name())
+		fmt.Printf("val: %p", val.Pointer)
+		return ty, val, nil
+	default:
+		return nil, reflect.Value{}, fmt.Errorf("not struct or pointer to struct: %d (%s.%s)", ty.Kind(), ty.PkgPath(), ty.Name())
+	}
+}
+
+func dumpOps (t *testing.T, ops []etcd.Op) {
+	for _, op := range ops {
+		if op.IsGet() {
+			key := string(op.KeyBytes())
+			s := fmt.Sprintf("%s %s", "GET", key)
+			t.Log(s)
+		} else if op.IsPut() {
+			key := string(op.KeyBytes())
+			val := string(op.ValueBytes())
+			s := fmt.Sprintf("%s %s %s", "PUT", key, val)
+			t.Log(s)
+		} else {
+			t.Logf("%#v", op)
+		}
+	}
+}
+
+func dumpStruct(t *testing.T, ty reflect.Type, val reflect.Value) {
+	for i := range ty.NumField() {
+		var sf reflect.StructField = ty.Field(i)
+		var sfv reflect.Value = val.Field(i)
+		fieldName := getFieldName(sf)
+		fieldValue, err := getFieldValue(sfv)
+		assert.NoError(t, err)
+		t.Logf("%s=%s", fieldName, fieldValue)
+	}
+
+}
+
+func testEncodeBool(t *testing.T, key string, b any) {
+	valExpected, err := json.Marshal(b)
+	require.NoError(t, err)
+	ops, err := Encode(newEcoContext(), key, b)
+	require.NoError(t, err)
+	dumpOps(t, ops)
+	require.NotEmpty(t, ops)
+	require.Equal(t, 1, len(ops))
+	op := ops[0]
+	require.NotEmpty(t, op)
+	require.True(t, op.IsPut())
+	assert.Equal(t, []byte(key), op.KeyBytes())
+	assert.Equal(t, valExpected, op.ValueBytes())
+}
+
+func testEncodeNumber(t *testing.T, key string, n any) {
+	valExpected, err := json.Marshal(n)
+	require.NoError(t, err)
+	ops, err := Encode(newEcoContext(), key, n)
+	require.NoError(t, err)
+	dumpOps(t, ops)
+	require.NotEmpty(t, ops)
+	require.Equal(t, 1, len(ops))
+	op := ops[0]
+	require.NotEmpty(t, op)
+	require.True(t, op.IsPut())
+	assert.Equal(t, []byte(key), op.KeyBytes())
+	assert.Equal(t, valExpected, op.ValueBytes())
+}
+
+func testEncodeString(t *testing.T, key string, s string) {
+	ops, err := Encode(newEcoContext(), key, s)
+	dumpOps(t, ops)
+	// valExpected := fmt.Sprintf(`"%s"`, s)
+	require.NoError(t, err)
+	require.NotEmpty(t, ops)
+	require.Equal(t, 1, len(ops))
+	// op := ops[0]
+	// require.NotEmpty(t, op)
+	// require.True(t, op.IsPut())
+	// assert.Equal(t, []byte(key), op.KeyBytes())
+	// assert.Equal(t, []byte(valExpected), op.ValueBytes())
 }
