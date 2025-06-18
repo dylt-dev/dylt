57f77a0a0ff6c33e449fbeaec3476807bb6b6a63
Chris Lalos <chris.lalos@gmail.com>
2025-06-17 19:22:33 -0500

    `dylt` (its a start)

--- scripts/bash-completion/_daylight.sh
+++ scripts/bash-completion/_daylight.sh
@@ -4,7 +4,7 @@
 
 # Various bash completion values, global variables, etc. Useful for development/debugging.
 status () {
-	printf '%-16s: DONE=%d N=%d COMP_CWORD=%d cur=[%s] <%s>\n' "$1()" "$DONE" "$N" "$COMP_CWORD" "${COMP_WORDS[COMP_CWORD]}" "${COMP_WORDS[*]}" >> /tmp/dylt.log
+	printf '%-16s: DONE=%d N=%d COMP_CWORD=%d COMP_TYPE=%d cur=[%s] <%s>\n' "$1()" "$DONE" "$N" "$COMP_CWORD" "$COMP_TYPE" "${COMP_WORDS[COMP_CWORD]}" "${COMP_WORDS[*]}" >> /tmp/dylt.log
 }
 
 ### Main script
@@ -20,8 +20,21 @@ _dylt () {
 }
 
 
+# dylt
 on-main () {
-
+    # anything goes
+    argvals=(call config get host init list misc vm watch)
+    flags=()
+    get-token token
+    status 
+    if on-last-token; then
+        comment "$(printf "latest token; complete with flags or subcommands")"
+        case $token in
+            -*)	complete-with-words	"${flags[*]}"	"$token";;
+            *)	complete-with-words	"${argvals[*]}"	"$token";;
+        esac
+        return
+    fi
 }
 
 # Register completion handler with function
