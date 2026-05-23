3d7e39336e3aaa5751a10d59729df8c785191dbc
Chris Lalos <chris.lalos@gmail.com>
2026-05-23 13:06:17 -0500

    hoisted common stuff into DeepType + introduced DeepSubType

--- common/deeptype.go
+++ common/deeptype.go
@@ -24,10 +24,19 @@ import (
 */
 
 type DeepType struct{ typ reflect.Type }
-type ScalarType DeepType
+
+// type ScalarType DeepType
 type MapType DeepType
 type SliceType DeepType
 type StructType DeepType
+
+type DeepSubType interface {
+	emitKey()
+	keyName() string
+	keyType() DeepType
+	nextType() DeepType
+	zeroValue() string
+}
 
 func NewDeepType(typ reflect.Type) DeepType {
 	switch NewFlavor(typ.Kind()) {
@@ -49,13 +58,13 @@ func NewMapType(dt DeepType) MapType {
 	return MapType(dt)
 }
 
-func NewScalarType(dt DeepType) ScalarType {
-	if dt.Flavor() != Scalar {
-		panic(fmt.Errorf("expecting map (%s)", dt.Flavor().String()))
-	}
+// func NewScalarType(dt DeepType) ScalarType {
+// 	if dt.Flavor() != Scalar {
+// 		panic(fmt.Errorf("expecting map (%s)", dt.Flavor().String()))
+// 	}
 
-	return ScalarType(dt)
-}
+// 	return ScalarType(dt)
+// }
 
 func NewSliceType(dt DeepType) SliceType {
 	if dt.Flavor() != Slice {
@@ -73,34 +82,54 @@ func NewStructType(dt DeepType) StructType {
 	return StructType(dt)
 }
 
-
 func (dt DeepType) EmitFieldRef() {
-	switch dt.Flavor() {
-	case Map:
-		NewMapType(dt).EmitFieldRef()
-	case Scalar:
-		NewScalarType(dt).EmitFieldRef()
-	case Slice:
-		NewSliceType(dt).EmitFieldRef()
-	case Struct:
-		NewStructType(dt).EmitFieldRef()
-	default:
-		panic(fmt.Errorf("unsupported type (%s)", dt.typ))
+	// switch dt.Flavor() {
+	// case Map:
+	// 	NewMapType(dt).EmitFieldRef()
+	// case Slice:
+	// 	NewSliceType(dt).EmitFieldRef()
+	// case Struct:
+	// 	NewStructType(dt).EmitFieldRef()
+	// default:
+	// 	panic(fmt.Errorf("unsupported type (%s)", dt.typ))
+	// }
+	dt.subType().emitKey()
+	if !dt.isScalar() {
+		dt.subType().nextType().EmitFieldRef()
 	}
 }
 
 func (dt DeepType) EmitTree(plevel *int) {
-	switch dt.Flavor() {
-	case Map:
-		NewMapType(dt).EmitTree(plevel)
-	case Scalar:
-		NewScalarType(dt).EmitTree(plevel)
-	case Slice:
-		NewSliceType(dt).EmitTree(plevel)
-	case Struct:
-		NewStructType(dt).EmitTree(plevel)
-	default:
-		panic(fmt.Errorf("unsupported type (%s)", dt.typ))
+	// switch dt.Flavor() {
+	// case Map:
+	// 	NewMapType(dt).EmitTree(plevel)
+	// // case Scalar:
+	// // 	NewScalarType(dt).EmitTree(plevel)
+	// case Slice:
+	// 	NewSliceType(dt).EmitTree(plevel)
+	// case Struct:
+	// 	NewStructType(dt).EmitTree(plevel)
+	// default:
+	// 	panic(fmt.Errorf("unsupported type (%s)", dt.typ))
+	// }
+	if dt.isScalar() {
+		// if dt.subType().nextType().typ.Kind() == reflect.String {
+		if dt.subType().keyType().typ.Kind() == reflect.String {
+			fmt.Printf("tree%d := NewValueTree(ctx, %q, %s)\n", *plevel, dt.subType().keyName(), dt.subType().zeroValue())
+		} else {
+			fmt.Printf("tree%d := NewValueTree(ctx, %s, %s)\n", *plevel, dt.subType().keyName(), dt.subType().zeroValue())
+		}
+	} else {
+		dt.subType().nextType().EmitTree(plevel)
+		*plevel++
+		var keyName string
+		if dt.subType().keyType().typ.Kind() == reflect.String {
+			keyName = fmt.Sprintf("%q", dt.subType().keyName())
+		} else {
+			keyName = dt.subType().keyName()
+		}
+
+		fmt.Printf("tree%d := NewValueTree(ctx, %s, tree%d)\n", *plevel, keyName, *plevel-1)
 	}
 }
 
@@ -108,126 +137,185 @@ func (dt DeepType) Flavor() Flavor {
 	return NewFlavor(dt.typ.Kind())
 }
 
-func (t MapType) EmitFieldRef() {
+func (dt DeepType) isScalar() bool {
+	return dt.subType().nextType().Flavor() == Scalar
+}
+
+func (dt DeepType) subType() DeepSubType {
+	switch dt.Flavor() {
+	case Map:
+		return MapType(dt)
+	case Slice:
+		return SliceType(dt)
+	case Struct:
+		return StructType(dt)
+	default:
+		panic(fmt.Errorf("unsupported type (%s)", dt.typ))
+	}
+}
+
+// func (t MapType) EmitFieldRef() {
+// 	// x.Data[2]["bar"].Slice[0]["foo"].Val[3].N
+// 	t.emitKey()
+// 	if !t.IsScalar() {
+// 		t.nextType().EmitFieldRef()
+// 	}
+// }
+
+// func (t MapType) EmitTree(plevel *int) {
+// 	if t.IsScalar() {
+// 		if t.typ.Elem().Kind() == reflect.String {
+// 			fmt.Printf("tree%d := NewValueTree(ctx, \"%s\", %q)\n", *plevel, t.keyName(), t.ZeroValue())
+// 		} else {
+// 			fmt.Printf("tree%d := NewValueTree(ctx, \"%s\", %s)\n", *plevel, t.keyName(), t.ZeroValue())
+// 		}
+// 	} else {
+// 		NewDeepType(t.typ.Elem()).EmitTree(plevel)
+// 		*plevel++
+// 		fmt.Printf("tree%d := NewValueTree(ctx, \"%s\", tree%d)\n", *plevel, t.keyName(), *plevel-1)
+// 	}
+// }
+
+// func (t MapType) IsScalar() bool {
+// 	return t.nextType().Flavor() == Scalar
+// }
+
+func (t MapType) emitKey() {
 	// x.Data[2]["bar"].Slice[0]["foo"].Val[3].N
 	if t.typ.Key().Kind() == reflect.String {
-		fmt.Printf("[%q]", t.KeyName())
+		fmt.Printf("[%q]", t.keyName())
 	} else {
-		fmt.Printf("[%s]", t.KeyName())
-	}
-	if !t.IsScalar() {
-		NewDeepType(t.typ.Elem()).EmitFieldRef()
+		fmt.Printf("[%s]", t.keyName())
 	}
 }
 
-func (t MapType) EmitTree(plevel *int) {
-	if t.IsScalar() {
-		if t.typ.Elem().Kind() == reflect.String {
-			fmt.Printf("tree%d := NewValueTree(ctx, \"%s\", %q)\n", *plevel, t.KeyName(), t.ZeroValue())
-		} else {
-			fmt.Printf("tree%d := NewValueTree(ctx, \"%s\", %s)\n", *plevel, t.KeyName(), t.ZeroValue())
-		}
-	} else {
-		NewDeepType(t.typ.Elem()).EmitTree(plevel)
-		*plevel++
-		fmt.Printf("tree%d := NewValueTree(ctx, \"%s\", tree%d)\n", *plevel, t.KeyName(), *plevel-1)
-	}
+func (t MapType) keyType() DeepType {
+	return NewDeepType(t.typ.Key())
 }
 
-func (t MapType) IsScalar() bool {
-	return NewDeepType(t.typ.Elem()).Flavor() == Scalar
+func (t MapType) keyName() string {
+	return fmt.Sprintf("%s", reflect.Zero(t.typ.Key()))
+	// buf, err := json.Marshal(reflect.Zero(t.typ.Key()).Interface())
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// return fmt.Sprintf("%s", string(buf))
 }
 
-func (t MapType) KeyName() string {
-	return fmt.Sprintf("%v", reflect.Zero(t.typ.Key()).Interface())
+func (t MapType) nextType() DeepType {
+	return NewDeepType(t.typ.Elem())
 }
 
-func (t MapType) ZeroValue() string {
-	return fmt.Sprintf("%v", reflect.Zero(t.typ.Elem()))
+func (t MapType) zeroValue() string {
+	return fmt.Sprintf("%v", reflect.Zero(t.nextType().typ))
 }
 
-func (t SliceType) EmitFieldRef() {
-	// x.Data[2]["bar"].Slice[0]["foo"].Val[3].N
+// func (t ScalarType) EmitTree(plevel *int) {
+// }
+
+// func (t ScalarType) KeyName() {
+
+// }
+
+// func (t ScalarType) ZeroValue() reflect.Value {
+// 	return reflect.Zero(t.typ)
+// }
+
+// func (t ScalarType) EmitFieldRef() {
+// }
+
+// func (t SliceType) EmitFieldRef() {
+// 	// x.Data[2]["bar"].Slice[0]["foo"].Val[3].N
+// 	t.emitKey()
+// 	if !t.IsScalar() {
+// 		t.nextType().EmitFieldRef()
+// 	}
+// }
+
+// func (t SliceType) EmitTree(plevel *int) {
+// 	if t.IsScalar() {
+// 		if t.typ.Elem().Kind() == reflect.String {
+// 			fmt.Printf("tree%d := NewValueTree(ctx, \"%s\", %q)\n", *plevel, t.KeyName(), t.ZeroValue())
+// 		} else {
+// 			fmt.Printf("tree%d := NewValueTree(ctx, \"%s\", %s)\n", *plevel, t.KeyName(), t.ZeroValue())
+// 		}
+// 	} else {
+// 		NewDeepType(t.typ.Elem()).EmitTree(plevel)
+// 		*plevel++
+// 		fmt.Printf("tree%d := NewValueTree(ctx, \"%s\", tree%d)\n", *plevel, t.KeyName(), *plevel-1)
+// 	}
+// }
+
+// func (t SliceType) IsScalar() bool {
+// 	return t.nextType().Flavor() == Scalar
+// }
+
+func (t SliceType) emitKey() {
 	if t.typ.Elem().Kind() == reflect.String {
-		fmt.Printf("[%q]", t.KeyName())
+		fmt.Printf("[%q]", t.keyName())
 	} else {
-		fmt.Printf("[%s]", t.KeyName())
-	}
-	if !t.IsScalar() {
-		NewDeepType(t.typ.Elem()).EmitFieldRef()
+		fmt.Printf("[%s]", t.keyName())
 	}
 }
 
-func (t ScalarType) EmitTree(plevel *int) {
+func (t SliceType) keyType() DeepType {
+	return NewDeepType(reflect.TypeFor[int]())
 }
 
-func (t ScalarType) KeyName() {
-
-}
-
-func (t ScalarType) ZeroValue() reflect.Value {
-	return reflect.Zero(t.typ)
-}
-
-func (t ScalarType) EmitFieldRef() {
-}
-
-func (t SliceType) EmitTree(plevel *int) {
-	if t.IsScalar() {
-		if t.typ.Elem().Kind() == reflect.String {
-			fmt.Printf("tree%d := NewValueTree(ctx, \"%s\", %q)\n", *plevel, t.KeyName(), t.ZeroValue())
-		} else {
-			fmt.Printf("tree%d := NewValueTree(ctx, \"%s\", %s)\n", *plevel, t.KeyName(), t.ZeroValue())
-		}
-	} else {
-		NewDeepType(t.typ.Elem()).EmitTree(plevel)
-		*plevel++
-		fmt.Printf("tree%d := NewValueTree(ctx, \"%s\", tree%d)\n", *plevel, t.KeyName(), *plevel-1)
-	}
-}
-
-func (t SliceType) IsScalar() bool {
-	return NewDeepType(t.typ.Elem()).Flavor() == Scalar
-}
-
-func (t SliceType) KeyName() string {
+func (t SliceType) keyName() string {
 	return "0"
 }
 
-func (t SliceType) ZeroValue() string {
-	return fmt.Sprintf("%v", reflect.Zero(t.typ.Elem()).Interface())
+func (t SliceType) nextType() DeepType {
+	return NewDeepType(t.typ.Elem())
 }
 
-func (t StructType) EmitFieldRef() {
-	// x.Data[2]["bar"].Slice[0]["foo"].Val[3].N
-	fmt.Printf(".%s", t.KeyName())
-	if !t.IsScalar() {
-		NewDeepType(t.typ.Field(0).Type).EmitFieldRef()
-	}
+func (t SliceType) zeroValue() string {
+	return fmt.Sprintf("%v", reflect.Zero(t.nextType().typ))
 }
 
-func (t StructType) EmitTree(plevel *int) {
-	if t.IsScalar() {
-		if t.typ.Field(0).Type.Kind() == reflect.String {
-			fmt.Printf("tree%d := NewValueTree(ctx, \"%s\", %q)\n", *plevel, t.KeyName(), t.ZeroValue())
-		} else {
-			fmt.Printf("tree%d := NewValueTree(ctx, \"%s\", %s)\n", *plevel, t.KeyName(), t.ZeroValue())
-		}
-	} else {
-		NewDeepType(t.typ.Field(0).Type).EmitTree(plevel)
-		*plevel++
-		fmt.Printf("tree%d := NewValueTree(ctx, \"%s\", tree%d)\n", *plevel, t.KeyName(), *plevel-1)
-	}
+// func (t StructType) EmitFieldRef() {
+// 	// x.Data[2]["bar"].Slice[0]["foo"].Val[3].N
+// 	t.emitKey()
+// 	if !t.IsScalar() {
+// 		t.nextType().EmitFieldRef()
+// 	}
+// }
+
+// func (t StructType) EmitTree(plevel *int) {
+// 	if t.IsScalar() {
+// 		if t.typ.Field(0).Type.Kind() == reflect.String {
+// 			fmt.Printf("tree%d := NewValueTree(ctx, \"%s\", %q)\n", *plevel, t.KeyName(), t.ZeroValue())
+// 		} else {
+// 			fmt.Printf("tree%d := NewValueTree(ctx, \"%s\", %s)\n", *plevel, t.KeyName(), t.ZeroValue())
+// 		}
+// 	} else {
+// 		NewDeepType(t.typ.Field(0).Type).EmitTree(plevel)
+// 		*plevel++
+// 		fmt.Printf("tree%d := NewValueTree(ctx, \"%s\", tree%d)\n", *plevel, t.KeyName(), *plevel-1)
+// 	}
+// }
+
+// func (t StructType) IsScalar() bool {
+// 	return t.nextType().Flavor() == Scalar
+// }
+
+func (t StructType) emitKey() {
+	fmt.Printf(".%s", t.keyName())
 }
 
-func (t StructType) IsScalar() bool {
-	return NewDeepType(t.typ.Field(0).Type).Flavor() == Scalar
+func (t StructType) keyName() string {
+	return fmt.Sprintf("%s", t.typ.Field(0).Name)
 }
 
-func (t StructType) KeyName() string {
-	return t.typ.Field(0).Name
+func (t StructType) keyType() DeepType {
+	return NewDeepType(reflect.TypeFor[string]())
 }
 
-func (t StructType) ZeroValue() string {
-	return fmt.Sprintf("%v", reflect.Zero(t.typ.Field(0).Type).Interface())
+func (t StructType) nextType() DeepType {
+	return NewDeepType(t.typ.Field(0).Type)
+}
+
+func (t StructType) zeroValue() string {
+	return fmt.Sprintf("%v", reflect.Zero(t.nextType().typ).Interface())
 }
--- common/deeptype_test.go
+++ common/deeptype_test.go
@@ -10,238 +10,239 @@ import (
 
 func TestMapTypeEmitFieldRef1(t *testing.T) {
 	type m map[bool]int
-	mapType := NewMapType(NewDeepType(reflect.TypeFor[m]()))
+	// mapType := NewDeepType(reflect.TypeFor[m]())
+	mapType := NewDeepType(reflect.TypeFor[m]())
 	mapType.EmitFieldRef()
 }
 
 func TestMapTypeEmitFieldRef2(t *testing.T) {
 	type m map[int]string
-	mapType := NewMapType(NewDeepType(reflect.TypeFor[m]()))
+	mapType := NewDeepType(reflect.TypeFor[m]())
 	mapType.EmitFieldRef()
 }
 
 func TestMapTypeEmitFieldRef3(t *testing.T) {
 	type m map[string]bool
-	mapType := NewMapType(NewDeepType(reflect.TypeFor[m]()))
+	mapType := NewDeepType(reflect.TypeFor[m]())
 	mapType.EmitFieldRef()
 }
 
 func TestMapTypeEmitFieldRef4(t *testing.T) {
 	type m map[string][]int
-	mapType := NewMapType(NewDeepType(reflect.TypeFor[m]()))
+	mapType := NewDeepType(reflect.TypeFor[m]())
 	mapType.EmitFieldRef()
 }
 func TestSliceTypeEmitFieldRef1(t *testing.T) {
 	type sl []bool
-	mapType := NewSliceType(NewDeepType(reflect.TypeFor[sl]()))
+	mapType := NewDeepType(reflect.TypeFor[sl]())
 	mapType.EmitFieldRef()
 }
 
 func TestSliceTypeEmitFieldRef2(t *testing.T) {
 	type sl []int
-	mapType := NewSliceType(NewDeepType(reflect.TypeFor[sl]()))
+	mapType := NewDeepType(reflect.TypeFor[sl]())
 	mapType.EmitFieldRef()
 }
 
 func TestSliceTypeEmitFieldRef3(t *testing.T) {
 	type sl []string
-	mapType := NewSliceType(NewDeepType(reflect.TypeFor[sl]()))
+	mapType := NewDeepType(reflect.TypeFor[sl]())
 	mapType.EmitFieldRef()
 }
 
 func TestStructTypeEmitFieldRef1(t *testing.T) {
-	type st struct{BoolValue bool}
-	mapType := NewStructType(NewDeepType(reflect.TypeFor[st]()))
+	type st struct{ BoolValue bool }
+	mapType := NewDeepType(reflect.TypeFor[st]())
 	mapType.EmitFieldRef()
 }
 
 func TestStructTypeEmitFieldRef2(t *testing.T) {
-	type st struct{IntValue int}
-	mapType := NewStructType(NewDeepType(reflect.TypeFor[st]()))
+	type st struct{ IntValue int }
+	mapType := NewDeepType(reflect.TypeFor[st]())
 	mapType.EmitFieldRef()
 }
 
 func TestStructTypeEmitFieldRef3(t *testing.T) {
-	type st struct{StringValue string}
-	mapType := NewStructType(NewDeepType(reflect.TypeFor[st]()))
+	type st struct{ StringValue string }
+	mapType := NewDeepType(reflect.TypeFor[st]())
 	mapType.EmitFieldRef()
 }
 
 func TestMapTypeEmitTree1(t *testing.T) {
 	type m map[string]int
-	mapType := NewMapType(NewDeepType(reflect.TypeFor[m]()))
+	mapType := NewDeepType(reflect.TypeFor[m]())
 	n := 0
 	mapType.EmitTree(&n)
 }
 
 func TestMapTypeEmitTree2(t *testing.T) {
 	type m map[string]string
-	mapType := NewMapType(NewDeepType(reflect.TypeFor[m]()))
+	mapType := NewDeepType(reflect.TypeFor[m]())
 	n := 0
 	mapType.EmitTree(&n)
 }
 
 func TestMapTypeEmitTree3(t *testing.T) {
 	type m map[string]bool
-	mapType := NewMapType(NewDeepType(reflect.TypeFor[m]()))
+	mapType := NewDeepType(reflect.TypeFor[m]())
 	n := 0
 	mapType.EmitTree(&n)
 }
 
 func TestMapTypeEmitTree4(t *testing.T) {
 	type m map[string][]int
-	mapType := NewMapType(NewDeepType(reflect.TypeFor[m]()))
+	mapType := NewDeepType(reflect.TypeFor[m]())
 	n := 0
 	mapType.EmitTree(&n)
 }
 func TestMapTypeIsScalar1(t *testing.T) {
 	type m map[string]int
-	mapType := NewMapType(NewDeepType(reflect.TypeFor[m]()))
-	require.True(t, mapType.IsScalar())
+	mapType := NewDeepType(reflect.TypeFor[m]())
+	require.True(t, mapType.isScalar())
 }
 
 func TestMapTypeIsScalar2(t *testing.T) {
 	type m map[string]struct{}
-	mapType := NewMapType(NewDeepType(reflect.TypeFor[m]()))
-	require.False(t, mapType.IsScalar())
+	mapType := NewDeepType(reflect.TypeFor[m]())
+	require.False(t, mapType.isScalar())
 }
 
 func TestMapTypeKeyName1(t *testing.T) {
 	type m map[string]int
-	mapType := NewMapType(NewDeepType(reflect.TypeFor[m]()))
-	require.Equal(t, "", mapType.KeyName())
+	mapType := NewDeepType(reflect.TypeFor[m]())
+	require.Equal(t, "", mapType.subType().keyName())
 }
 
 func TestMapTypeKeyName2(t *testing.T) {
 	type m map[bool]int
-	mapType := NewMapType(NewDeepType(reflect.TypeFor[m]()))
-	require.Equal(t, "false", mapType.KeyName())
+	mapType := NewDeepType(reflect.TypeFor[m]())
+	require.Equal(t, "false", mapType.subType().keyName())
 }
 
 func TestMapTypeKeyName3(t *testing.T) {
 	type m map[int]int
-	mapType := NewMapType(NewDeepType(reflect.TypeFor[m]()))
-	require.Equal(t, "0", mapType.KeyName())
+	mapType := NewDeepType(reflect.TypeFor[m]())
+	require.Equal(t, "0", mapType.subType().keyName())
 }
 
 func TestMapTypeZeroValue1(t *testing.T) {
 	type m map[string]string
-	mapType := NewMapType(NewDeepType(reflect.TypeFor[m]()))
-	require.Equal(t, "", mapType.ZeroValue())
+	mapType := NewDeepType(reflect.TypeFor[m]())
+	require.Equal(t, "", mapType.subType().zeroValue())
 }
 
 func TestMapTypeZeroValue2(t *testing.T) {
 	type m map[string]bool
-	mapType := NewMapType(NewDeepType(reflect.TypeFor[m]()))
-	require.Equal(t, "false", mapType.ZeroValue())
+	mapType := NewDeepType(reflect.TypeFor[m]())
+	require.Equal(t, "false", mapType.subType().zeroValue())
 }
 
 func TestMapTypeZeroValue3(t *testing.T) {
 	type m map[string]int
-	mapType := NewMapType(NewDeepType(reflect.TypeFor[m]()))
-	require.Equal(t, "0", mapType.ZeroValue())
+	mapType := NewDeepType(reflect.TypeFor[m]())
+	require.Equal(t, "0", mapType.subType().zeroValue())
 }
 
 func TestSliceTypeIsScalar1(t *testing.T) {
 	type sl []int
-	sliceType := NewSliceType(NewDeepType(reflect.TypeFor[sl]()))
-	require.True(t, sliceType.IsScalar())
+	sliceType := NewDeepType(reflect.TypeFor[sl]())
+	require.True(t, sliceType.isScalar())
 }
 
 func TestSliceTypeIsScalar2(t *testing.T) {
 	type sl []struct{}
-	sliceType := NewSliceType(NewDeepType(reflect.TypeFor[sl]()))
-	require.False(t, sliceType.IsScalar())
+	sliceType := NewDeepType(reflect.TypeFor[sl]())
+	require.False(t, sliceType.isScalar())
 }
 
 func TestSliceTypeKeyName1(t *testing.T) {
 	type sl []int
-	sliceType := NewSliceType(NewDeepType(reflect.TypeFor[sl]()))
-	require.Equal(t, "0", sliceType.KeyName())
+	sliceType := NewDeepType(reflect.TypeFor[sl]())
+	require.Equal(t, "0", sliceType.subType().keyName())
 }
 
 func TestSliceTypeKeyName2(t *testing.T) {
 	type sl []string
-	sliceType := NewSliceType(NewDeepType(reflect.TypeFor[sl]()))
-	require.Equal(t, "0", sliceType.KeyName())
+	sliceType := NewDeepType(reflect.TypeFor[sl]())
+	require.Equal(t, "0", sliceType.subType().keyName())
 }
 
 func TestSliceTypeKeyName3(t *testing.T) {
 	type sl []bool
-	sliceType := NewSliceType(NewDeepType(reflect.TypeFor[sl]()))
-	require.Equal(t, "0", sliceType.KeyName())
+	sliceType := NewDeepType(reflect.TypeFor[sl]())
+	require.Equal(t, "0", sliceType.subType().keyName())
 }
 
 func TestSliceTypeKeyName4(t *testing.T) {
 	type sl []struct{}
-	sliceType := NewSliceType(NewDeepType(reflect.TypeFor[sl]()))
-	require.Equal(t, "0", sliceType.KeyName())
+	sliceType := NewDeepType(reflect.TypeFor[sl]())
+	require.Equal(t, "0", sliceType.subType().keyName())
 }
 
 func TestSliceTypeZeroValue1(t *testing.T) {
 	type sl []string
-	sliceType := NewSliceType(NewDeepType(reflect.TypeFor[sl]()))
-	require.Equal(t, "", sliceType.ZeroValue())
+	sliceType := NewDeepType(reflect.TypeFor[sl]())
+	require.Equal(t, "", sliceType.subType().zeroValue())
 }
 
 func TestSliceTypeZeroValue2(t *testing.T) {
 	type sl []bool
-	sliceType := NewSliceType(NewDeepType(reflect.TypeFor[sl]()))
-	require.Equal(t, "false", sliceType.ZeroValue())
+	sliceType := NewDeepType(reflect.TypeFor[sl]())
+	require.Equal(t, "false", sliceType.subType().zeroValue())
 }
 
 func TestSliceTypeZeroValue3(t *testing.T) {
 	type sl []int
-	sliceType := NewSliceType(NewDeepType(reflect.TypeFor[sl]()))
-	require.Equal(t, "0", sliceType.ZeroValue())
+	sliceType := NewDeepType(reflect.TypeFor[sl]())
+	require.Equal(t, "0", sliceType.subType().zeroValue())
 }
 
 func TestStructTypeIsScalar1(t *testing.T) {
 	type st struct{ Value int }
-	structType := NewStructType(NewDeepType(reflect.TypeFor[st]()))
-	require.True(t, structType.IsScalar())
+	structType := NewDeepType(reflect.TypeFor[st]())
+	require.True(t, structType.isScalar())
 }
 
 func TestStructTypeIsScalar2(t *testing.T) {
 	type st struct{ Value []int }
-	structType := NewStructType(NewDeepType(reflect.TypeFor[st]()))
-	require.False(t, structType.IsScalar())
+	structType := NewDeepType(reflect.TypeFor[st]())
+	require.False(t, structType.isScalar())
 }
 
 func TestStructTypeKeyName1(t *testing.T) {
 	type st struct{ Value string }
-	structType := NewStructType(NewDeepType(reflect.TypeFor[st]()))
-	require.Equal(t, "Value", structType.KeyName())
+	structType := NewDeepType(reflect.TypeFor[st]())
+	require.Equal(t, "Value", structType.subType().keyName())
 }
 
 func TestStructTypeKeyName2(t *testing.T) {
 	type st struct{ Value int }
-	structType := NewStructType(NewDeepType(reflect.TypeFor[st]()))
-	require.Equal(t, "Value", structType.KeyName())
+	structType := NewDeepType(reflect.TypeFor[st]())
+	require.Equal(t, "Value", structType.subType().keyName())
 }
 
 func TestStructTypeKeyName3(t *testing.T) {
 	type st struct{ Value struct{} }
-	structType := NewStructType(NewDeepType(reflect.TypeFor[st]()))
-	require.Equal(t, "Value", structType.KeyName())
+	structType := NewDeepType(reflect.TypeFor[st]())
+	require.Equal(t, "Value", structType.subType().keyName())
 }
 
 func TestStructTypeZeroValue1(t *testing.T) {
 	type st struct{ Value bool }
-	structType := NewStructType(NewDeepType(reflect.TypeFor[st]()))
-	require.Equal(t, "false", structType.ZeroValue())
+	structType := NewDeepType(reflect.TypeFor[st]())
+	require.Equal(t, "false", structType.subType().zeroValue())
 }
 
 func TestStructTypeZeroValue2(t *testing.T) {
 	type st struct{ Value int }
-	structType := NewStructType(NewDeepType(reflect.TypeFor[st]()))
-	require.Equal(t, "0", structType.ZeroValue())
+	structType := NewDeepType(reflect.TypeFor[st]())
+	require.Equal(t, "0", structType.subType().zeroValue())
 }
 
 func TestStructTypeZeroValue3(t *testing.T) {
 	type st struct{ Value string }
-	structType := NewStructType(NewDeepType(reflect.TypeFor[st]()))
-	require.Equal(t, "", structType.ZeroValue())
+	structType := NewDeepType(reflect.TypeFor[st]())
+	require.Equal(t, "", structType.subType().zeroValue())
 }
 
 func TestEmitFieldRef(t *testing.T) {
@@ -255,7 +256,9 @@ func TestEmitFieldRef(t *testing.T) {
 	fmt.Print("x")
 	DeepType{typ}.EmitFieldRef()
 	fmt.Println()
+
 }
+
 func TestEmitTree(t *testing.T) {
 	type deepType struct {
 		Data []map[string]struct {
