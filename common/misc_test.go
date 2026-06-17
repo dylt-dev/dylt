3c339b099c29de7983fff44e23bdc9fd4963ce94
Chris Lalos <chris.lalos@gmail.com>
2026-06-17 00:30:35 -0500

    looking good

--- common/misc_test.go
+++ common/misc_test.go
@@ -10,6 +10,48 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+
+func TestLev1 (t *testing.T) {
+	Lev("aaa", "bbbbb")
+}
+
+
+func TestLev2 (t *testing.T) {
+	n := Lev("abcde", "12345")
+	t.Logf("n = %d", n)
+}
+
+
+func TestLev3 (t *testing.T) {
+	n := Lev("cat", "dog")
+	t.Logf("n = %d", n)
+}
+
+
+func TestLev4 (t *testing.T) {
+	n := Lev("cat", "rat")
+	t.Logf("n = %d", n)
+}
+
+
+func TestLev5 (t *testing.T) {
+	n := Lev("cat", "cab")
+	t.Logf("n = %d", n)
+}
+
+
+func TestLev6 (t *testing.T) {
+	n := Lev("cat", "cot")
+	t.Logf("n = %d", n)
+}
+
+
+func TestLev7 (t *testing.T) {
+	n := Lev("cat", "cat")
+	t.Logf("n = %d", n)
+}
+
+
 func TestWriteDeclaration(t *testing.T) {
 	envWriteDecl, is := os.LookupEnv("ECOGEN")
 	if !is || (envWriteDecl != "1" && strings.ToLower(envWriteDecl) != "y") {
